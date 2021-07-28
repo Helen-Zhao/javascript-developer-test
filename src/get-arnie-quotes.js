08d91b2751ac11159ea8cdc4f844059452554f32
Helen Zhao <helen.zhao1@yahoo.co.nz>
2021-07-28 20:41:52 +1000

    Helen Zhao - Smokeball dev test

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,39 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const results = [];
+  const responseKey = "Arnie Quote"; // Note: Keys can be hardcoded, split out for better modifiability
+  const failureKey = "FAILURE";
+
+  const promises = urls.map(url => {
+    return httpGet(url);
+  })
+
+  const handleSuccess = (resp) => {
+    const result = {
+      [responseKey]: JSON.parse(resp?.body).message,
+    }; 
+    results.push(result);
+   }
+  
+   const handleError = (err) => {
+    const result = {
+      [failureKey]: JSON.parse(err?.body).message,
+    };
+    results.push(result);     
+   }
+
+  await Promise.all(promises).then(responses => {
+    responses.forEach(resp => {
+      if (resp.status == 200) {
+        handleSuccess(resp);
+      } else {
+        handleError(resp);
+      }
+    })
+  }); 
+
+  return results;
 };
 
 module.exports = {
